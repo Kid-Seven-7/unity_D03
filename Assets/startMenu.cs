31941944730002c589c5eb0b9bcbd47998c9fdf9
Joseph L. Ngoma <testing.jo.wtc@gmail.com>
2018-10-17 10:53:38 +0200

    init

---
+++ Assets/startMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class startMenu : MonoBehaviour {
+
+	public Button play, exit;
+	
+	// Update is called once per frame
+	void Update (){
+		play.onClick.AddListener(playOnClick);
+		exit.onClick.AddListener(exitOnClick);
+	}
+
+	void playOnClick(){
+		SceneManager.LoadScene("ex01");
+	}
+
+	void exitOnClick(){
+		Application.Quit();
+	}
+}
